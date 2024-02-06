b6fbe384c77cc4f97b4d3faeb4d66cb462fddcbd
renan823 <renan.trofino@gmail.com>
2024-02-06 19:22:20 -0300

    feat: lesson modal

--- renderer/.next/static/chunks/webpack.js
+++ renderer/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e7ace02213ca6ae5"; }
+/******/ 		__webpack_require__.h = function() { return "d67b76e213cd52b7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- renderer/components/LessonModal.tsx
+++ renderer/components/LessonModal.tsx
@@ -1,19 +1,49 @@
 import { useForm } from "react-hook-form";
 import Modal from "./Modal";
+import { Lesson } from "../intefaces";
+import Field from "./Field";
+import { useState } from "react";
+
+const SearchUser = ({ setUser }) => {
+    return (
+        <div>
+            <input type="search" name="" id="" />
+            <div>
+
+            </div>
+        </div>
+    )
+}
 
 export default function LessonModal ({ isOpen, closeModal, lesson }) {
     let defaultValues = {};
+
+    const [user, setUser] = useState(null);
 
     if (lesson) {
         defaultValues = {};
     }
 
-    const { control, handleSubmit } = useForm();
+    const { control, handleSubmit, reset, formState: { errors, isSubmitting } } = useForm<Lesson>({ defaultValues });
+
+    const onSubmit = (newLesson) => {
+        console.log(newLesson);
+    }
 
     return (
         <div>
             <Modal isOpen={isOpen} closeModal={closeModal} title={lesson ? "Dados da aula" : "Nova aula"}>
-                <h1>Oii</h1>
+                <div>
+                    <form className="overflow-auto h-96 p-4" onSubmit={handleSubmit(onSubmit)}>
+                        <SearchUser setUser={setUser}/>
+                        <Field name="startAt" control={control} rules={{ required: true }} error={ errors.startAt } label="Início da aula" type="datetime-local"/>
+                        <Field name="endAt" control={control} rules={{ required: true }} error={ errors.endAt } label="Fim da aula" type="datetime-local"/>
+                        <Field name="value" control={control} rules={{ required: true }} error={ errors.value } label="Valor da aula" type="number" step={5}/>
+                        <div className="w-full p-2">
+                            <button type="submit">{ isSubmitting ? "Enviando" :  lesson ? "Alterar": "Salvar" }</button>
+                        </div>
+                    </form>
+                </div>
             </Modal>
         </div>
     )
